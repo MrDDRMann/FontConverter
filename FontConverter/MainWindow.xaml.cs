fe0493fd39de975174cb8108795f3e42d1c91720
Alexander Spindler <spindler.alexander@outlook.com>
2017-02-24 14:36:09 +0100

    Add sln

---
+++ FontConverter/MainWindow.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Application = System.Windows.Application;
+using Cursors = System.Windows.Input.Cursors;
+using FontStyle = System.Drawing.FontStyle;
+using MessageBox = System.Windows.MessageBox;
+
+namespace FontConverter
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Methods _methods = new Methods();
+        private readonly List<FontStyle> _fontStyle = new List<FontStyle>();
+        private List<FontFamily> _fonts;
+        private Color _forground;
+        private Color _background;
+        private int _fontsize;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbFonts_Loaded(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            InstalledFontCollection installedFontCollection = new InstalledFontCollection();
+            _fonts = installedFontCollection.Families.ToList();
+            foreach (FontFamily font in _fonts)
+            {
+                CbFonts.Items.Add(font.Name);
+            }
+            int index = CbFonts.Items.IndexOf("Segoe MDL2 Assets");
+            CbFonts.SelectedIndex = index == -1 ? 0 : index;
+            Mouse.OverrideCursor = null;
+        }
+
+        private void tbPath_Loaded(object sender, RoutedEventArgs e)
+        {
+            TbPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        private void btOpen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TbPath.Text != "")
+                {
+                    Process.Start("explorer.exe", TbPath.Text);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Can't open folder. Please check your path.\n" + exc.Message, "FontConverter Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void btConvert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Mouse.OverrideCursor = Cursors.Wait;
+                string path = _methods.CreatePath(_fonts, CbFonts.SelectedIndex, TbPath.Text, _fontsize, _fontStyle[CbStyle.SelectedIndex]);
+                char[] allChar = _methods.GetAllChar(_fonts, CbFonts.SelectedIndex);
+                _methods.DrawImage(_fonts, CbFonts.SelectedIndex, path, _fontsize, _fontStyle[CbStyle.SelectedIndex], _background, _forground, allChar);
+                Mouse.OverrideCursor = null;
+            }
+            catch (Exception exc)
+            {
+                Mouse.OverrideCursor = null;
+                MessageBox.Show("Can't convert font to picture\n" + exc.Message, "FontConverter Error", MessageBoxButton.OK);
+            }
+        }
+
+        private
+            void MiClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown
+                ();
+        }
+
+        private void CpBackground_OnSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<System.Windows.Media.Color?> e)
+        {
+            _background
+                =
+                Color.FromArgb
+                    (
+                        CpBackground.SelectedColor.Value.A
+                        ,
+                        CpBackground.SelectedColor.Value.R
+                        ,
+                        CpBackground.SelectedColor.Value.G
+                        ,
+                        CpBackground.SelectedColor.Value.B
+                    );
+        }
+
+        private void CpForeground_OnSelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<System.Windows.Media.Color?> e)
+        {
+            _forground
+                =
+                Color.FromArgb
+                    (
+                        CpForeground.SelectedColor.Value.A
+                        ,
+                        CpForeground.SelectedColor.Value.R
+                        ,
+                        CpForeground.SelectedColor.Value.G
+                        ,
+                        CpForeground.SelectedColor.Value.B
+                    );
+        }
+
+        private void UdSize_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (
+                UdSize.Value == null)
+            {
+                _fontsize
+                    = 50;
+            }
+            else
+            {
+                _fontsize
+                    = (
+                        int
+                        )
+                        UdSize.Value;
+            }
+        }
+
+        private void CpForeground_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _forground = Color.FromArgb(CpForeground.SelectedColor.Value.A, CpForeground.SelectedColor.Value.R,
+                CpForeground.SelectedColor.Value.G, CpForeground.SelectedColor.Value.B);
+        }
+
+        private void CpBackground_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _background = Color.FromArgb(CpBackground.SelectedColor.Value.A, CpBackground.SelectedColor.Value.R,
+                CpBackground.SelectedColor.Value.G, CpBackground.SelectedColor.Value.B);
+        }
+
+        private void UdSize_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _fontsize = 50;
+        }
+
+        private void MiAbout_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FontConverter Version 1.2 \nNovember 2016\nAlexander Spindler", " About FontConverter",
+                MessageBoxButton.OK);
+        }
+
+        private void BtBrowse_OnClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog objDialog = new FolderBrowserDialog
+            {
+                Description = "Opening save path for pictures",
+                SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            DialogResult objResult = objDialog.ShowDialog();
+            if (objResult == System.Windows.Forms.DialogResult.OK)
+            {
+                TbPath.Text = objDialog.SelectedPath;
+            }
+        }
+
+        private void CbStyle_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            CbStyle.Items.Add(System.Drawing.FontStyle.Regular.ToString());
+            _fontStyle.Add(System.Drawing.FontStyle.Regular);
+            CbStyle.Items.Add(System.Drawing.FontStyle.Bold.ToString());
+            _fontStyle.Add(System.Drawing.FontStyle.Bold);
+            CbStyle.Items.Add(System.Drawing.FontStyle.Italic.ToString());
+            _fontStyle.Add(System.Drawing.FontStyle.Italic);
+            CbStyle.Items.Add(System.Drawing.FontStyle.Strikeout.ToString());
+            _fontStyle.Add(System.Drawing.FontStyle.Strikeout);
+            CbStyle.Items.Add(System.Drawing.FontStyle.Underline.ToString());
+            _fontStyle.Add(System.Drawing.FontStyle.Underline);
+            CbStyle.SelectedIndex = 0;
+        }
+    }
+}
---
+++ FontConverter/Methods.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using Color = System.Drawing.Color;
+using FontFamily = System.Drawing.FontFamily;
+using FontStyle = System.Drawing.FontStyle;
+
+namespace FontConverter
+{
+    internal class Methods
+    {
+        public SizeF GetStringSize(string value, Font font, Graphics g)
+        {
+            return g.MeasureString(value, font);
+        }
+
+        public char[] GetAllChar(List<FontFamily> fontsFamilies, int index)
+        {
+            System.Windows.Media.FontFamily mfont = new System.Windows.Media.FontFamily(fontsFamilies[index].Name);
+            Typeface typeface = new Typeface(mfont,
+                FontStyles.Italic,
+                FontWeights.Normal,
+                FontStretches.Normal);
+            GlyphTypeface glyph;
+            if (!typeface.TryGetGlyphTypeface(out glyph))
+                throw new InvalidOperationException("No glyphtypeface found");
+            IDictionary<int, ushort> glyphs = glyph.CharacterToGlyphMap;
+            char[] allChar = new char[glyphs.Count];
+            for (int i = 0; i < glyphs.Count; i++)
+            {
+                allChar[i] = Convert.ToChar(glyphs.Keys.ElementAt(i));
+            }
+            return allChar;
+        }
+
+        public Bitmap ResizeBitmap(Bitmap sourceBmp, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+                g.DrawImage(sourceBmp, 0, 0, width, height);
+            return result;
+        }
+
+        public string CreatePath(List<FontFamily> fontsFamilies, int index, string path, int fontSize, FontStyle fontStyle)
+        {
+            path = path + "\\" + fontsFamilies[index].Name.Replace(" ", "") + "-" + fontSize + "-" + fontStyle;
+            bool exists = Directory.Exists(path);
+
+            if (!exists)
+                Directory.CreateDirectory(path);
+            return path;
+        }
+
+        public void DrawImage(List<FontFamily> fontsFamilies, int index, string path, int fontsize, FontStyle fontStyle, Color background,
+                Color forground, char[] allChar)
+        {
+            int i = 0;
+            foreach (char printableChar in allChar)
+            {
+                using (Bitmap b = new Bitmap(500, 500))
+                {
+                    using (Graphics g = Graphics.FromImage(b))
+                    {
+                        g.Clear(background);
+                        using (
+                            Font font = new Font(fontsFamilies[index], fontsize, fontStyle,
+                                GraphicsUnit.Pixel))
+                        {
+                            SizeF size = GetStringSize(printableChar.ToString(), font, g);
+                            using (Bitmap bitmap = new Bitmap((int) size.Width, (int) size.Height))
+                            {
+                                using (Graphics graphics = Graphics.FromImage(bitmap))
+                                {
+                                    graphics.Clear(background);
+                                    using (SolidBrush solidBrush = new SolidBrush(forground))
+                                    {
+                                        graphics.InterpolationMode = InterpolationMode.High;
+                                        graphics.CompositingQuality = CompositingQuality.HighQuality;
+                                        graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                                        graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+                                        graphics.DrawString(printableChar.ToString(), font, solidBrush,
+                                            new PointF(0, 0));
+                                        bitmap.Save(path + "\\" + i + ".png", ImageFormat.Png);
+                                        bitmap.Dispose();
+                                        graphics.Dispose();
+                                    }
+                                }
+                            }
+                        }
+                        b.Dispose();
+                        g.Dispose();
+                    }
+                }
+                i++;
+            }
+        }
+    }
+}
